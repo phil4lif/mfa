68b8c0ecc72938c7d6061a703540cb5555a4f729
Phil <philipjcowan1@gmail.com>
2022-11-10 08:57:09 -0700

    rails app with users

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save {  self.email = email.downcase }
+  has_many :articles
+  validates :username, presence: true,
+            uniqueness: true,
+            length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: true,
+            format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+end
